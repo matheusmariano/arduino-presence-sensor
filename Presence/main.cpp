3b0fa4c21ca86d0019445afb168a22995140e9fd
Matheus Mariano <vmatheus.mariano@gmail.com>
2018-07-18 21:02:23 -0300

    Initial Commit

---
+++ Presence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Presence
+//
+//  Created by Matheus Mariano on 18/07/18.
+//  Copyright © 2018 Matheus Mariano. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
